06b72b7c56562d5bdb527a270e10e284cb0cb341
Lanka <lanka@minthrm.com>
2017-08-23 11:17:49 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <c-input type="number" v-model="firstName"></c-input>
+        {{ firstName }}
+    </div>
+</template>
+
+<script>
+    import Input from  './components/form/Input.vue'
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+                firstName:''
+            }
+        },
+
+        components:{
+            cInput:Input
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/form/Input.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="input-group">
+        <input v-if="type === 'text'" type="text" v-model="inputValue">
+        <input v-if="type === 'number'" type="number" v-model="inputValue">
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: {
+            type:{
+                type:String,
+                default:'text'
+            }
+        },
+        
+        data(){
+            return {
+                inputValue: '',
+                myType:''
+            }
+        },
+
+        watch: {
+            inputValue(){
+                this.$emit('input', this.inputValue)
+            }
+        },
+
+
+    }
+</script>

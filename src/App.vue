ab0d416f62261b1d61d26ac8be7e73fddd87974d
Dlanka <darshaka4mail@gmail.com>
2017-08-24 05:45:00 +0530

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
+
     <div id="app">
-        <c-input max="2" type="text" v-model="firstName" label="Number Label"></c-input>
-        <c-input type="text" v-model="firstName2" label="Number Label"></c-input>
-        {{ firstName }}
+
+        <header>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                </a>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <section>
+            <router-view></router-view>
+            <c-input max="2" type="text" v-model="firstName" label="Number Label"></c-input>
+            <c-input type="text" v-model="firstName2" label="Number Label"></c-input>
+        </section>
+
     </div>
 </template>
 
 <script>
-    import Input from './components/form/Input.vue'
 
     export default {
         name: 'app',
@@ -25,5 +50,6 @@
 </script>
 
 <style lang="scss">
+    @import "scss/main.scss";
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Input from './components/form/Input.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+
+    {path: '/form', component: Input}
+]
+
+const router = new VueRouter({
+    routes
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
